98a1bd2423003fc77d2a9d812cd42079a4d394b6
martinwheeler <martin@martinwheeler.com.au>
2018-07-28 17:04:51 +1000

    Add zsh-completions install

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,7 @@ function doIt() {
   if [ -d "${HOME}/.oh-my-zsh" ] && [ ! -d "${HOME}/.oh-my-zsh/custom/themes/powerlevel9k" ];
     then
       git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k;
+      git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     fi
 
   # Copy dotfiles
