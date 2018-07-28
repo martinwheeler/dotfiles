a8563e7938f7d000e432ffc13be09366f9957a50
Martin <martin@Martin-is-in-Deep-Space-Cool.local>
2018-07-28 16:23:34 +1000

    Add file and directory checks

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,11 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-git pull origin master;
+# git pull origin master;
 
 function doIt() {
   # PowerLevel9k
-  if [ -d "~/.oh-my-zsh" && ! -d "~/.oh-my-zsh/custom/themes/powerlevel9k" ]; then git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k; fi
+  if [ -d "~/.oh-my-zsh" ] && [ ! -d "~/.oh-my-zsh/custom/themes/powerlevel9k" ]; then git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k; fi
 
   # Copy dotfiles
   rsync --exclude ".git/" \
@@ -26,6 +26,7 @@ if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt;
