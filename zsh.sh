c62251942a806c2383108c4a861a89e6b378ac63
Martin <martin@Martin-is-in-Deep-Space-Cool.local>
2018-07-28 16:25:07 +1000

    Add sanity check for zsh install

--- zsh.sh
+++ zsh.sh
@@ -1,2 +1,2 @@
 # Oh-my-Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [ ! -d "~/.oh-my-zsh" ]; then sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" fi
