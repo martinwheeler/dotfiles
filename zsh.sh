bdc8dc8a8467e6d22e9f3b91da345da4724503a4
Martin <martin@Martin-is-in-Deep-Space-Cool.local>
2018-07-28 16:42:19 +1000

    Fix zsh sanity check with HOME env

--- zsh.sh
+++ zsh.sh
@@ -1,2 +1,5 @@
 # Oh-my-Zsh
-if [ ! -d "~/.oh-my-zsh" ]; then sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" fi
+if [ ! -d "${HOME}/.oh-my-zsh" ];
+then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
